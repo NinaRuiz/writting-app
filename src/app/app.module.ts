cab22e61cd18ae7045afe78a141e55f10fd384f1
Nina Ruiz <nina.ruizandres@gmail.com>
2020-06-21 06:22:09 +0200

    Create editor-page component.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { EditorPageComponent } from './pages/editor-page/editor-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EditorPageComponent
   ],
   imports: [
     BrowserModule,
