45bf4970ce931a38717b325132224c7cb115b205
Nina Ruiz <nina.ruizandres@gmail.com>
2020-06-21 06:40:25 +0200

    Create editor card on EditorPageComponent.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,8 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { EditorPageComponent } from './pages/editor-page/editor-page.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatCardModule} from '@angular/material/card';
 
 @NgModule({
   declarations: [
@@ -12,7 +14,9 @@ import { EditorPageComponent } from './pages/editor-page/editor-page.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatCardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
