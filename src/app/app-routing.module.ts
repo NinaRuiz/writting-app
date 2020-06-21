665a4f8b8913aa30da8ea9b64c7e07478f26cdb1
Nina Ruiz <nina.ruizandres@gmail.com>
2020-06-21 06:23:49 +0200

    Add route for EditorPageComponent.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {EditorPageComponent} from './pages/editor-page/editor-page.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'editor', component: EditorPageComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
